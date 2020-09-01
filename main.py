509083084661740140da11c5a08474fe3ba71b99
Ryan (Geonhee) Son <ryan.son1002@gmail.com>
2020-09-01 18:28:30 +0900

    Created web page and added search function

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
-from indeed import get_jobs as get_indeed_jobs
-from so import get_jobs as get_so_jobs
-from save import save_to_file
+from flask import Flask, render_template, request, redirect, send_file
+from scrapper import get_jobs
+from exporter import save_to_file
 
-so_jobs = get_so_jobs()
-indeed_jobs = get_indeed_jobs()
-jobs = so_jobs + indeed_jobs
-save_to_file(jobs)
+app = Flask("SuperScrapper")
+
+db = {}
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get("word")
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+        "report.html", searchingBy=word, resultsNumber=len(jobs), jobs=jobs)
+
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get("word")
+    if not word:
+      raise Exception()
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file("jobs.csv", as_attachment=True)
+  except:
+    return redirect("/")    
+
+app.run(host="0.0.0.0")
